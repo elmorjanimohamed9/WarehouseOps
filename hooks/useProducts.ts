1ed255cb20ece21b02e60b213a49c72d966bb190
Mohamed El Morjani <elmorjanimohamed9@gmail.com>
2025-02-14 21:18:24 +0100

    feat(WAR-8): integrate product creation with backend API

---
+++ hooks/useProducts.ts
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/store/store';
+import {
+  fetchProducts,
+  fetchProductById,
+  addNewProduct,
+  updateExistingProduct,
+  clearSelectedProduct,
+  clearError,
+} from '@/store/slices/productsSlice';
+import { Product } from '@/types/product';
+
+export const useProducts = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const {
+    items,
+    selectedProduct,
+    loading,
+    error,
+  } = useSelector((state: RootState) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  return {
+    products: items,
+    selectedProduct,
+    loading,
+    error,
+    refreshProducts: () => dispatch(fetchProducts()),
+    getProduct: (id: string) => dispatch(fetchProductById(id)),
+    addProduct: (product: Partial<Product>) => dispatch(addNewProduct(product)),
+    updateProduct: (id: string, updates: Partial<Product>) => 
+      dispatch(updateExistingProduct({ id, updates })),
+    clearProduct: () => dispatch(clearSelectedProduct()),
+    clearError: () => dispatch(clearError()),
+  };
+};
